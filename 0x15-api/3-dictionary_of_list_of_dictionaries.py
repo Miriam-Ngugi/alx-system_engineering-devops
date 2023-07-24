b2cfb45f4ae52d67153460665bd1399ac8269094
Miriam-Ngugi <waitherangugi34@gmail.com>
2023-07-24 19:40:11 +0000

    Exports to do list info for all employees to JSON format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+# Module that calls RESTful API and exports to JSON file for all users
 import json
 import requests
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+    '''Gives name of employee and completed tasks and exports as JSON file
+    for all users
+    '''
+    url = "https://jsonplaceholder.typicode.com/users"
+    req = requests.get(url)
+    jreq = req.json()
+    id_dict = {}
+    id_name = {}
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    for i in jreq:
+        id_dict.update({i['id']: []})
+        id_name.update({i['id']: i['username']})
+    url = "https://jsonplaceholder.typicode.com/todos"
+    req = requests.get(url)
+    jreq = req.json()
+    tasks = []
+    tasks_dict = []
+    for k in id_dict.keys():
+        for i in jreq:
+
+            if k == i['userId']:
+                tasks_dict.append({'task': i['title'],
+                                   'completed': i['completed'],
+                                   'username': id_name[k]})
+                continue
+        id_dict.update({k: tasks_dict})
+        tasks_dict = []
+    with open('todo_all_employees.json', mode='w') as json_file:
+        json.dump(id_dict, json_file)
